9fb996c008ae801278a940845d6d0c50a1b17f60
王杰 <wangjie3763@boe.com.cn>
2025-10-27 11:36:36 +0800

    feat: 修改site端口

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '~/types'
 
 const config: SiteConfig = {
   // Absolute URL to the root of your published site, used for generating links and sitemaps.
-  site: 'http://iwangjie.top:5173',
+  site: 'http://iwangjie.top:4321',
   // The name of your site, used in the title and for SEO.
   title: 'IWangJie',
   // The description of your site, used for SEO and RSS feed.
